3729050ef22ce44771b069c989fcb87f201c7f55
Ole Jørgen <oj.espeland@stud.uis.no>
2024-02-14 21:15:37 +0100

    Added support for two different pdf parsers, as well as general code clean up

--- helpers.py
+++ helpers.py
@@ -1,4 +1,24 @@
+import cv2
+import numpy as np
 import pandas as pd
 
 def read_solution(filename: str) -> pd.DataFrame:
     return pd.read_csv("sample_data_solutions/" + filename)
+
+# Deskew image for ocr parse.
+def deskew(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(gray)
+    coords = np.column_stack(np.where(gray > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
--- main.py
+++ main.py
@@ -1,41 +1,89 @@
+from typing import Callable, Any
 from elasticsearch import Elasticsearch
+import numpy as np
+from pdf2image import convert_from_path
 import spacy
 import PyPDF2
 import pandas as pd
+import pytesseract
+import sys
+from helpers import deskew
 
-nlp = spacy.load("nb_core_news_lg")
+def geoparse_pdf(
+        file_path: str,
+        pdf_parser: Callable[[str], str], # pypdf2 or ocr
+        is_wikipedia: bool # Is the pdf a wikipedia article? 
+) -> None:
+    text = pdf_parser(file_path, is_wikipedia)
+    # print(text)
+    geoparse(text)
 
-# filename = "Temaanalyse om dødsulykker i tunnel[671].pdf"
-# filename = "kjoretoybranner_norske_vegtunneler_til_2021_nævestad_2[670].pdf"
-# filename = "varsom_snoeskred_rapport2023_32[678].pdf"
-filename = "Karolinernes_dødsmarsj.pdf"
+def pypdf2_parse(
+        file_path: str,
+        is_wikipedia: bool # Is the pdf a wikipedia article? 
+) -> str:
+    with open(file_path, "rb") as file:
+        pdfReader = PyPDF2.PdfReader(file, strict=True)
+        text = ""
+        for page in pdfReader.pages:
+            text += page.extract_text() + "\n\n"
+        # TODO: Potential preprocessing for pypdf
+        return text.strip()
 
-with open("sample_data/" + filename, "rb") as file:
-    pdfReader = PyPDF2.PdfReader(file)
+def ocr_parse(
+        file_path: str,
+        is_wikipedia: bool # Is the pdf a wikipedia article? 
+) -> str:
+    pages = convert_from_path(file_path)
+    text = ""
+    for page in pages:
+        text += pytesseract.image_to_string(deskew(np.array(page)), lang="nor")
+
+    # Some custom logic to help with wikipedia articles 
+    if is_wikipedia:
+        text_split = text.split("\n")
+        for i, line in enumerate(text_split):
+            # TODO: This can lead to mistakes if these strings are in an article without being in the footer.
+            if line == "Litteratur" or line == "Referanser" or line == "Eksterne lenker":
+                text = "\n".join(text_split[:i])
+    return text.strip()
+
+def geoparse(
+        text: str
+) -> None:
     df = pd.DataFrame([], columns=["name", "label"])
+    nlp = spacy.load("nb_core_news_lg")
+    doc = nlp(text)
+    places = [[ent.text.strip(), ent.label_] for ent in doc.ents if ent.label_ in ["GPE", "LOC", "GPE_LOC", "GPE_ORG"]]
+    for place in places:
+        name = place[0]
 
-    for page in pdfReader.pages:
-        page_text = page.extract_text()
-        doc = nlp(page_text)
-        places = [[ent.text.strip(), ent.label_] for ent in doc.ents if ent.label_ in ["GPE", "LOC", "GPE_LOC", "GPE_ORG"]]
-        for place in places:
-            name = place[0]
+        # Check for names that are obviously not places
+        if not name.isalpha() or name.isspace() or name.islower():
+            continue
+        
+        # Check if place has already been added
+        if name in df["name"].tolist():
+            continue 
+        
+        df.loc[len(df.index)] = place
 
-            # Check for names that are obviously not places
-            if not name.isalpha() or name.isspace() or name.islower():
-                continue
-            
-            # Check if place has already been added
-            if name in df["name"].tolist():
-                continue 
-            
-            df.loc[len(df.index)] = place
+    es = Elasticsearch("http://localhost:9200")
 
+    # Iterate through found place names and query ES.
+    # for _, row in df.iterrows():
+    #     query(es, row["name"])
+    print(df)
 
-es = Elasticsearch("http://localhost:9200")
-for _, row in df.iterrows():
-    print(row["name"])
-    search = es.search(index="geonames", query={"match": {"name": row["name"]}})
+def query(
+        es: Elasticsearch,
+        placename: str
+) -> None:
+    search = es.search(index="geonames", query={"match": {"name": placename}})
     for result in search["hits"]["hits"]:
         print(result["_source"])
-    print()
+        pass
+
+if __name__ == "__main__":
+    file_path = str(sys.argv[1]) 
+    geoparse_pdf(file_path, pdf_parser=ocr_parse, is_wikipedia=True)
